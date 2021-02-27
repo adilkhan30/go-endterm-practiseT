ffa0b2d01a729ed7158fb5ab97b8a9a3cf9df298
Adilek550 <58088550+Adilek550@users.noreply.github.com>
2021-02-27 17:17:32 +0600

    calccalc

---
+++ calc/server/server.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"go-endterm-practiseT/calc/greet/calculatorpb"
+	"google.golang.org/grpc"
+	"io"
+	"log"
+	"net"
+)
+
+type server struct {
+	calculatorpb.UnimplementedCalculatorServiceServer
+}
+
+func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositionRequest, stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
+	fmt.Printf("received PrimeNumberDecomposition RPC: %v\n", req)
+	num := req.GetNumber()
+	divisor := int64(2)
+
+	for num > 1 {
+		if num%divisor == 0 {
+			stream.Send(&calculatorpb.PrimeNumberDecompositionResponse{
+				PrimeFactor: divisor,
+			})
+			num = num / divisor
+		} else {
+			divisor++
+			log.Printf("divisor increased to %v", divisor)
+		}
+	}
+
+	return nil
+}
+
+func (*server) ComputeAverage(stream calculatorpb.CalculatorService_ComputeAverageServer) error {
+	fmt.Println("received ComputeAverage RPC")
+
+	sum := int32(0)
+	count := 0
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			average := float64(sum) / float64(count)
+			return stream.SendAndClose(&calculatorpb.ComputeAverageResponse{
+				Average: average,
+			})
+		}
+		if err != nil {
+			log.Fatalf("error client stream: %v", err)
+		}
+
+		sum += req.GetNumber()
+		count++
+	}
+}
+
+func main() {
+	fmt.Println("running grpc on port 4000...")
+	lis, err := net.Listen("tcp", "0.0.0.0:4000")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("error serve: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module endtermPrimeComposition
+
+go 1.15
+
+require (
+	google.golang.org/grpc v1.36.0
+	google.golang.org/protobuf v1.25.0
+)
